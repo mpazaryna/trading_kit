b7019aabb453f541934ae3d57dd74d2da52106c4
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-10-02 11:41:31 -0400

    refactor: function expects pure data, not panda

--- src/trading_kit/utils/visualization.py
+++ src/trading_kit/utils/visualization.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional, Union
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,18 +7,27 @@ from trading_kit.exceptions import InvalidDataError, InvalidThresholdError
 
 
 def plot_line_chart(
-    data: pd.Series,
+    data: Union[List[float], List[int]],
+    x_values: Optional[List[Union[float, int, str]]] = None,
     title: str = "Line Chart",
     xlabel: str = "X-axis",
     ylabel: str = "Y-axis",
     save_path: Optional[str] = None,
 ) -> None:
     """Plot a line chart for the given data and optionally save it to a file."""
-    if not isinstance(data, pd.Series):
-        raise InvalidDataError("Input data must be a pandas Series.")
+    if not isinstance(data, list) or not all(isinstance(x, (int, float)) for x in data):
+        raise InvalidDataError("Input data must be a list of numbers.")
+
+    # Convert data to pandas Series
+    if x_values is None:
+        series = pd.Series(data)
+    else:
+        if len(x_values) != len(data):
+            raise InvalidDataError("x_values must have the same length as data.")
+        series = pd.Series(data, index=x_values)
 
     plt.figure(figsize=(10, 5))
-    plt.plot(data, marker="o")
+    plt.plot(series, marker="o")
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
